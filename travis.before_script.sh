4d5abcfd01ecd883896781c2e96a6b6d9252a8dc
Weston Ruter <weston@xwp.co>
2014-12-03 09:52:01 +1100

    Improve readability of Bash scripts

--- travis.before_script.sh
+++ travis.before_script.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+# TODO: These should not override any existing environment variables
 export WP_TESTS_DIR=/tmp/wordpress-tests/
 export PLUGIN_DIR=$(pwd)
 export PLUGIN_SLUG=$(basename $(pwd) | sed 's/^wp-//')
@@ -16,12 +17,34 @@ export YUI_COMPRESSOR_CHECK=1
 export DISALLOW_EXECUTE_BIT=0
 export PATH_INCLUDES=./
 export WPCS_STANDARD=$(if [ -e phpcs.ruleset.xml ]; then echo phpcs.ruleset.xml; else echo WordPress-Core; fi)
-if [ -e .ci-env.sh ]; then source .ci-env.sh; fi
-if [ -e phpunit.xml ] || [ -e phpunit.xml.dist ]; then wget -O /tmp/install-wp-tests.sh https://raw.githubusercontent.com/wp-cli/wp-cli/master/templates/install-wp-tests.sh; bash /tmp/install-wp-tests.sh wordpress_test root '' localhost $WP_VERSION; cd /tmp/wordpress/wp-content/plugins; ln -s $PLUGIN_DIR $PLUGIN_SLUG; cd $PLUGIN_DIR; fi
+
+# Load a .ci-env.sh to override the above environment variables
+if [ -e .ci-env.sh ]; then
+	source .ci-env.sh
+fi
+
+# Install the WordPress Unit Tests
+if [ -e phpunit.xml ] || [ -e phpunit.xml.dist ]; then
+	wget -O /tmp/install-wp-tests.sh https://raw.githubusercontent.com/wp-cli/wp-cli/master/templates/install-wp-tests.sh
+	bash /tmp/install-wp-tests.sh wordpress_test root '' localhost $WP_VERSION
+	cd /tmp/wordpress/wp-content/plugins
+	ln -s $PLUGIN_DIR $PLUGIN_SLUG
+	cd $PLUGIN_DIR
+fi
+
+# Install PHP_CodeSniffer and the WordPress Coding Standards
 mkdir -p $PHPCS_DIR && curl -L https://github.com/$PHPCS_GITHUB_SRC/archive/$PHPCS_GIT_TREE.tar.gz | tar xvz --strip-components=1 -C $PHPCS_DIR
 mkdir -p $WPCS_DIR && curl -L https://github.com/$WPCS_GITHUB_SRC/archive/$WPCS_GIT_TREE.tar.gz | tar xvz --strip-components=1 -C $WPCS_DIR
 $PHPCS_DIR/scripts/phpcs --config-set installed_paths $WPCS_DIR
-npm install -g jshint
-if [ -e composer.json ]; then wget http://getcomposer.org/composer.phar && php composer.phar install --dev; fi
+
+# Install JSHint
+if ! command -v jshint >/dev/null 2>&1; then
+	npm install -g jshint
+fi
+
+# Install Composer
+if [ -e composer.json ]; then
+	wget http://getcomposer.org/composer.phar && php composer.phar install --dev
+fi
 
 set +e
