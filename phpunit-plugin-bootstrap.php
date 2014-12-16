62f33684d20aca3d8cc83233fa363645d87c4517
Weston Ruter <weston@xwp.co>
2014-12-16 22:57:58 +1100

    Improve plugin autoloader encapsulation

--- phpunit-plugin-bootstrap.php
+++ phpunit-plugin-bootstrap.php
@@ -10,12 +10,26 @@ if ( ! file_exists( $_tests_dir . '/includes/' ) ) {
 }
 require_once $_tests_dir . '/includes/functions.php';
 
-$_plugin_dir = dirname( __DIR__ );
-$_plugin_slug = basename( $_plugin_dir );
-$_plugin_file = sprintf( '%s/%s.php', $_plugin_dir, $_plugin_slug );
-if ( ! file_exists( $_plugin_file ) ) {
-	trigger_error( "Unable to locate plugin file at $_plugin_file", E_USER_ERROR );
+/**
+ * Locate plugin file and autoload
+ */
+function xwp_autoload_plugin() {
+	$plugin_dir = dirname( __DIR__ );
+	$plugin_slug = basename( $plugin_dir );
+	$plugin_file = sprintf( '%s/%s.php', $plugin_dir, $plugin_slug );
+	if ( ! file_exists( $plugin_file ) ) {
+		trigger_error( "Unable to locate plugin file at $plugin_file", E_USER_ERROR );
+	}
+
+	// Force vip-init.php to be loaded on VIP quickstart
+	if ( file_exists( WP_CONTENT_DIR . '/themes/vip/plugins/vip-init.php' ) ) {
+		require_once( WP_CONTENT_DIR . '/themes/vip/plugins/vip-init.php' );
+	}
+
+	// Load this plugin
+	require_once $plugin_file;
 }
+tests_add_filter( 'muplugins_loaded', 'xwp_autoload_plugin' );
 
 /**
  * Force plugins defined in a constant (supplied by phpunit.xml) to be active at runtime.
@@ -42,16 +56,5 @@ function xwp_filter_active_plugins_for_phpunit( $active_plugins ) {
 }
 tests_add_filter( 'site_option_active_sitewide_plugins', 'xwp_filter_active_plugins_for_phpunit' );
 tests_add_filter( 'option_active_plugins', 'xwp_filter_active_plugins_for_phpunit' );
-
-
-tests_add_filter( 'muplugins_loaded', function () use ( $_plugin_file ) {
-	// Force vip-init.php to be loaded on VIP quickstart
-	if ( file_exists( WP_CONTENT_DIR . '/themes/vip/plugins/vip-init.php' ) ) {
-		require_once( WP_CONTENT_DIR . '/themes/vip/plugins/vip-init.php' );
-	}
-
-	// Load this plugin
-	require_once $_plugin_file;
-} );
 
 require $_tests_dir . '/includes/bootstrap.php';
