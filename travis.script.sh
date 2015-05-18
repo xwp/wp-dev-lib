8455dee9e695e258b01874aa90ed800013330eb8
Weston Ruter <weston@xwp.co>
2015-05-18 13:08:47 -0700

    Consolidate file lists

--- travis.script.sh
+++ travis.script.sh
@@ -2,42 +2,37 @@
 
 set -e
 
-find $PATH_INCLUDES -name '*.js'  | sed 's:^\.//*::' | sort > /tmp/included-js-files
-find $PATH_INCLUDES -name '*.php' | sed 's:^\.//*::' | sort > /tmp/included-php-files
+find $PATH_INCLUDES -type f | sed 's:^\.//*::' | sort > /tmp/included-files
 
 if [ "$TRAVIS_PULL_REQUEST" != 'false' ] && [ "$LIMIT_TRAVIS_PR_CHECK_SCOPE" != '0' ]; then
-	git diff --diff-filter=AM --name-only $TRAVIS_BRANCH...$TRAVIS_COMMIT | grep -E '\.php$' | cat - | sort > /tmp/changed-php-files
-	git diff --diff-filter=AM --name-only $TRAVIS_BRANCH...$TRAVIS_COMMIT | grep -E '\.js$' | cat - | sort > /tmp/changed-js-files
+	git diff --diff-filter=AM --name-only $TRAVIS_BRANCH...$TRAVIS_COMMIT | cat - | sort > /tmp/changed-files
 
-	comm -12 /tmp/included-php-files /tmp/changed-php-files > /tmp/checked-php-files
-	comm -12 /tmp/included-js-files /tmp/changed-js-files > /tmp/checked-js-files
+	comm -12 /tmp/included-files /tmp/changed-files > /tmp/checked-files
 else
-	cp /tmp/included-js-files /tmp/checked-js-files
-	cp /tmp/included-php-files /tmp/checked-php-files
+	cp /tmp/included-files /tmp/checked-files
 fi
 
 echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
-echo "PHP Files to check:"
-cat /tmp/checked-php-files
-echo
-
-echo "JS Files to check:"
-cat /tmp/checked-js-files
+echo "Files to check:"
+cat /tmp/checked-files
 echo
 
 # Run PHP syntax check
-cat /tmp/checked-php-files | xargs --no-run-if-empty php -lf
+cat /tmp/checked-files | grep '.php' | xargs --no-run-if-empty php -lf
 
 # Run JSHint
-cat /tmp/checked-js-files | xargs --no-run-if-empty jshint $( if [ -e .jshintignore ]; then echo "--exclude-path .jshintignore"; fi )
+# TODO: Restrict to lines changed
+cat /tmp/checked-files | grep '.js' | xargs --no-run-if-empty jshint $( if [ -e .jshintignore ]; then echo "--exclude-path .jshintignore"; fi )
 
 # Run JSCS
 if [ -n "$JSCS_CONFIG" ] && [ -e "$JSCS_CONFIG" ]; then
-	cat /tmp/checked-js-files | xargs --no-run-if-empty jscs --verbose --config="$JSCS_CONFIG"
+	# TODO: Restrict to lines changed
+	cat /tmp/checked-files | grep '.js' | xargs --no-run-if-empty jscs --verbose --config="$JSCS_CONFIG"
 fi
 
 # Run PHP_CodeSniffer
-cat /tmp/checked-php-files | xargs --no-run-if-empty $PHPCS_DIR/scripts/phpcs -s --standard=$WPCS_STANDARD $(if [ -n "$PHPCS_IGNORE" ]; then echo --ignore=$PHPCS_IGNORE; fi)
+# TODO: Restrict to lines changed
+cat /tmp/checked-files | grep '.php' | xargs --no-run-if-empty $PHPCS_DIR/scripts/phpcs -s --standard=$WPCS_STANDARD $(if [ -n "$PHPCS_IGNORE" ]; then echo --ignore=$PHPCS_IGNORE; fi)
 
 # Run PHPUnit tests
 if [ -e phpunit.xml ] || [ -e phpunit.xml.dist ]; then
@@ -45,8 +40,8 @@ if [ -e phpunit.xml ] || [ -e phpunit.xml.dist ]; then
 fi
 
 # Run YUI Compressor Check
-if [ "$YUI_COMPRESSOR_CHECK" == 1 ] && [ 0 != $( cat /tmp/checked-js-files | wc -l ) ]; then
+if [ "$YUI_COMPRESSOR_CHECK" == 1 ] && [ 0 != $( cat /tmp/checked-files | grep '.js' | wc -l ) ]; then
 	YUI_COMPRESSOR_PATH=/tmp/yuicompressor-2.4.8.jar
 	wget -O "$YUI_COMPRESSOR_PATH" https://github.com/yui/yuicompressor/releases/download/v2.4.8/yuicompressor-2.4.8.jar
-	cat /tmp/checked-js-files | xargs --no-run-if-empty java -jar "$YUI_COMPRESSOR_PATH" -o /dev/null 2>&1
+	cat /tmp/checked-files | grep '.js' | xargs --no-run-if-empty java -jar "$YUI_COMPRESSOR_PATH" -o /dev/null 2>&1
 fi
