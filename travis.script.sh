c61307a89795cada46d367bd2904bfdc1fee61ef
Weston Ruter <weston@xwp.co>
2015-05-18 22:16:54 -0700

    Add parse-diff-ranges

--- travis.script.sh
+++ travis.script.sh
@@ -3,9 +3,9 @@
 set -e
 
 if [ "$TRAVIS_PULL_REQUEST" != 'false' ] && [ "$LIMIT_TRAVIS_PR_CHECK_SCOPE" != '0' ]; then
-	git diff --diff-filter=AM --name-only $TRAVIS_BRANCH...$TRAVIS_COMMIT -- $PATH_INCLUDES | cat - | sort > /tmp/checked-files
+	git diff --diff-filter=AM --no-prefix --unified=0 $TRAVIS_BRANCH...$TRAVIS_COMMIT -- $PATH_INCLUDES | php $DEV_LIB_PATH/parse-diff-ranges.php  > /tmp/checked-files
 else
-	find $PATH_INCLUDES -type f | sed 's:^\.//*::' | sort > /tmp/checked-files
+	find $PATH_INCLUDES -type f | sed 's:^\.//*::' > /tmp/checked-files
 fi
 
 echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
@@ -13,11 +13,21 @@ echo "Files to check:"
 cat /tmp/checked-files
 echo
 
+function remove_diff_range {
+	sed 's/:[0-9][0-9]*-[0-9][0-9]*$//' | sort | uniq
+}
+function filter_php_files {
+	 grep '.php'
+}
+function filter_js_files {
+	grep '.js'
+}
+
 # Run PHP syntax check
-cat /tmp/checked-files | grep '.php' | xargs --no-run-if-empty php -lf
+cat /tmp/checked-files | remove_diff_range | filter_php_files | xargs --no-run-if-empty php -lf
 
 # Run JSHint
-if ! cat /tmp/checked-files | grep '.js' | xargs --no-run-if-empty jshint --reporter=unix $( if [ -e .jshintignore ]; then echo "--exclude-path .jshintignore"; fi ) > /tmp/jshint-report; then
+if ! cat /tmp/checked-files | remove_diff_range | filter_js_files | xargs --no-run-if-empty jshint --reporter=unix $( if [ -e .jshintignore ]; then echo "--exclude-path .jshintignore"; fi ) > /tmp/jshint-report; then
 	echo "Here are the problematic JSHINT files:"
 	cat /tmp/jshint-report
 fi
@@ -25,12 +35,12 @@ fi
 # Run JSCS
 if [ -n "$JSCS_CONFIG" ] && [ -e "$JSCS_CONFIG" ]; then
 	# TODO: Restrict to lines changed (need an emacs/unix reporter)
-	cat /tmp/checked-files | grep '.js' | xargs --no-run-if-empty jscs --verbose --config="$JSCS_CONFIG"
+	cat /tmp/checked-files | remove_diff_range | filter_js_files | xargs --no-run-if-empty jscs --verbose --config="$JSCS_CONFIG"
 fi
 
 # Run PHP_CodeSniffer
 # TODO: Restrict to lines changed
-if ! cat /tmp/checked-files | grep '.php' | xargs --no-run-if-empty $PHPCS_DIR/scripts/phpcs -s --report-full --report-emacs=/tmp/phpcs-report --standard=$WPCS_STANDARD $(if [ -n "$PHPCS_IGNORE" ]; then echo --ignore=$PHPCS_IGNORE; fi); then
+if ! cat /tmp/checked-files | remove_diff_range | filter_php_files | xargs --no-run-if-empty $PHPCS_DIR/scripts/phpcs -s --report-full --report-emacs=/tmp/phpcs-report --standard=$WPCS_STANDARD $(if [ -n "$PHPCS_IGNORE" ]; then echo --ignore=$PHPCS_IGNORE; fi); then
 	echo "Here are the problematic PHPCS files:"
 	cat /tmp/phpcs-report
 fi
@@ -41,8 +51,8 @@ if [ -e phpunit.xml ] || [ -e phpunit.xml.dist ]; then
 fi
 
 # Run YUI Compressor Check
-if [ "$YUI_COMPRESSOR_CHECK" == 1 ] && [ 0 != $( cat /tmp/checked-files | grep '.js' | wc -l ) ]; then
+if [ "$YUI_COMPRESSOR_CHECK" == 1 ] && [ 0 != $( cat /tmp/checked-files | filter_js_files | wc -l ) ]; then
 	YUI_COMPRESSOR_PATH=/tmp/yuicompressor-2.4.8.jar
 	wget -O "$YUI_COMPRESSOR_PATH" https://github.com/yui/yuicompressor/releases/download/v2.4.8/yuicompressor-2.4.8.jar
-	cat /tmp/checked-files | grep '.js' | xargs --no-run-if-empty java -jar "$YUI_COMPRESSOR_PATH" -o /dev/null 2>&1
+	cat /tmp/checked-files | remove_diff_range | filter_js_files | xargs --no-run-if-empty java -jar "$YUI_COMPRESSOR_PATH" -o /dev/null 2>&1
 fi
