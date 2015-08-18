a3104743667bccc52b9c20ba264888c7a04b10f3
Weston Ruter <weston@xwp.co>
2015-08-18 16:05:51 -0700

    Remove verbose extraction of tarballs

--- travis.before_script.sh
+++ travis.before_script.sh
@@ -48,8 +48,8 @@ if [ -e phpunit.xml ] || [ -e phpunit.xml.dist ]; then
 fi
 
 # Install PHP_CodeSniffer and the WordPress Coding Standards
-mkdir -p $PHPCS_DIR && curl -L https://github.com/$PHPCS_GITHUB_SRC/archive/$PHPCS_GIT_TREE.tar.gz | tar xvz --strip-components=1 -C $PHPCS_DIR
-mkdir -p $WPCS_DIR && curl -L https://github.com/$WPCS_GITHUB_SRC/archive/$WPCS_GIT_TREE.tar.gz | tar xvz --strip-components=1 -C $WPCS_DIR
+mkdir -p $PHPCS_DIR && curl -L https://github.com/$PHPCS_GITHUB_SRC/archive/$PHPCS_GIT_TREE.tar.gz | tar xz --strip-components=1 -C $PHPCS_DIR
+mkdir -p $WPCS_DIR && curl -L https://github.com/$WPCS_GITHUB_SRC/archive/$WPCS_GIT_TREE.tar.gz | tar xz --strip-components=1 -C $WPCS_DIR
 $PHPCS_DIR/scripts/phpcs --config-set installed_paths $WPCS_DIR
 
 # Install JSHint
