a0c497202d1895ac967f2383c0ab9ea25956d2d0
Weston Ruter <weston@xwp.co>
2015-11-02 16:36:23 -0800

    Revert "Load project specific phpunit bootstrap if present"

--- phpunit-plugin-bootstrap.php
+++ phpunit-plugin-bootstrap.php
@@ -1,7 +1,4 @@
 <?php
-if ( file_exists( __DIR__ . '/../phpunit-plugin-bootstrap.project.php' ) ) {
-    require_once( __DIR__ . '/../phpunit-plugin-bootstrap.project.php' );
-}
 
 global $_plugin_file;
 
