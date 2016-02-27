a91777a20eab2eb808513d809e4a665b22b4a834
Derek Herman <derek@valendesigns.com>
2016-02-26 21:49:46 -0800

    PHP 5.5+ is the correct requirement

--- travis.after_script.sh
+++ travis.after_script.sh
@@ -4,7 +4,7 @@ set -e
 set -v
 
 function is_supported {
-	php -r 'exit( version_compare( PHP_VERSION, "5.6.0", ">=" ) ? 0 : 1 );'
+	php -r 'exit( version_compare( PHP_VERSION, "5.5.0", ">=" ) ? 0 : 1 );'
 }
 if is_supported && [ -s "$TEMP_DIRECTORY/paths-scope-php" ] && [ -e .coveralls.yml ]; then
 	php vendor/bin/coveralls;
