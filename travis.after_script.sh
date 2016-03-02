96e6a804caeff41653f71d8214bda57e60c732dc
Weston Ruter <weston@xwp.co>
2016-03-02 14:30:47 -0800

    Specify json_path for coveralls

--- travis.after_script.sh
+++ travis.after_script.sh
@@ -6,5 +6,5 @@ set -v
 if can_generate_coverage_clover && [ -s "$TEMP_DIRECTORY/paths-scope-php" ]; then
 	echo -n "Current directory: "
 	pwd
-	php vendor/bin/coveralls -vvv --coverage_clover "$TEMP_DIRECTORY/clover.xml";
+	php vendor/bin/coveralls -vvv --coverage_clover "$TEMP_DIRECTORY/clover.xml" --json_path "$TEMP_DIRECTORY/coveralls-upload.json"
 fi
