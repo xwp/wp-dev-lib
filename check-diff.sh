41a1a16c543297b168968f85640899c11bfec24f
Weston Ruter <weston@xwp.co>
2016-03-02 14:03:47 -0800

    Exclude nested phpunit for vendors

--- check-diff.sh
+++ check-diff.sh
@@ -601,9 +601,10 @@ function run_phpunit_travisci {
 		phpunit $(verbose_arg) $( if [ -n "$PHPUNIT_CONFIG" ]; then echo -c "$PHPUNIT_CONFIG"; fi ) --stop-on-failure $(coverage_clover)
 	fi
 
-	for nested_project in $( find $PATH_INCLUDES -mindepth 2 ! -path */dev-lib/* -name 'phpunit.xml*' | sed 's:/[^/]*$::' ); do
+	for nested_project in $( find $PATH_INCLUDES -mindepth 2 ! -path '*/dev-lib/*' ! -path '*/vendor/*' -name 'phpunit.xml*' | sed 's:/[^/]*$::' ); do
 		(
 			cd "$nested_project"
+			echo "Running PHPUnit in nested project: $nested_project"
 			phpunit --stop-on-failure
 		)
 	done
