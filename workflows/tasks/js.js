7cb7a13d02dd10b2def0b4aa06ae3fb1480789c6
Piotr Delawski <piotr.delawski@xwp.co>
2017-07-24 12:48:20 +0200

    Remove unneeded code in the `JS` task.

--- workflows/tasks/js.js
+++ workflows/tasks/js.js
@@ -1,27 +1,26 @@
 import { tasks, isProd, isDev, browserslist, cwd } from '../utils/get-config';
 import gulp from 'gulp';
-import mergeStream from 'merge-stream';
-import { join, resolve } from 'path';
+import { resolve } from 'path';
 import webpack from 'webpack';
 import webpackStream from 'webpack-stream';
-const ProgressBarPlugin = require('progress-bar-webpack-plugin')
-const {removeEmpty} = require('webpack-config-utils')
+import ProgressBarPlugin from 'progress-bar-webpack-plugin';
+import { removeEmpty } from 'webpack-config-utils';
 import plumber from 'gulp-plumber';
 
 if ( undefined !== tasks.js ) {
-	function fn() {
-		let jsTasks, jsTasksStream, babelifyOptions, esLintOptions = {};
+	let fn = function() {
+		let babelifyOptions, webpackConfig, esLintOptions = {};
 		const paths = tasks.js;
 
 		babelifyOptions = {
 			presets: [
-				["env", {
-					"targets": {
+				[ 'env', {
+					targets: {
 						browsers: browserslist
 					}
-				}]
+				} ]
 			]
-		}
+		};
 
 		// Avoid linting for the test environment
 		if ( isDev || isProd ) {
@@ -29,15 +28,15 @@ if ( undefined !== tasks.js ) {
 				test: /\.js$/,
 				loader: 'eslint-loader',
 				exclude: /(node_modules)/
-			}
+			};
 		}
 
-		const webpackConfig = {
+		webpackConfig = {
 			context: resolve( cwd, paths.base ),
 			entry: paths.entry,
 			output: {
 				filename: '[name].js',
-				pathinfo: true == isDev,
+				pathinfo: isDev,
 			},
 			devtool: isProd ? 'source-map': 'eval',
 			module: {
@@ -59,21 +58,14 @@ if ( undefined !== tasks.js ) {
 			]),
 			watch: true,
 			cache: true,
-		}
+		};
 
-		const webpackJs = function( task ) {
-			return gulp.src( resolve( cwd, paths.base ) )
-				.pipe( plumber() )
-				.pipe( webpackStream( webpackConfig, webpack ) )
-				.pipe( plumber.stop() )
-				.pipe( gulp.dest( resolve( cwd, paths.dest ) ) );
-		}
-
-		jsTasks       = Array.isArray( tasks.js ) ? tasks.js : [ tasks.js ];
-		jsTasksStream = jsTasks.map( webpackJs );
-
-		return mergeStream( jsTasksStream );
-	}
+		return gulp.src( resolve( cwd, paths.base ) )
+			.pipe( plumber() )
+			.pipe( webpackStream( webpackConfig, webpack ) )
+			.pipe( plumber.stop() )
+			.pipe( gulp.dest( resolve( cwd, paths.dest ) ) );
+	};
 
 	fn.displayName = 'js-compile';
 
