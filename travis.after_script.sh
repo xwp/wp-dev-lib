89e8f71b6958a6b25451a3806eb2f6d7651eacab
Derek Herman <derek@valendesigns.com>
2017-12-20 19:10:45 -0800

    Add support for Coveralls 2.x.x

--- travis.after_script.sh
+++ travis.after_script.sh
@@ -5,6 +5,18 @@ set -v
 
 if can_generate_coverage_clover && [ -s "$TEMP_DIRECTORY/paths-scope-php" ] && [ -n "$INITIAL_DIR" ]; then
 	cd "$INITIAL_DIR"
-	php vendor/bin/coveralls -vvv
+
+	# Coveralls 1.x.x path
+	COVERALLS_BIN="vendor/bin/coveralls";
+
+	# Coveralls 2.x.x path
+	if [ ! -e "$COVERALLS_BIN" ]; then
+		COVERALLS_BIN="vendor/bin/php-coveralls"
+	fi
+
+	if [ -e "$COVERALLS_BIN" ]; then
+	    php $COVERALLS_BIN -vvv
+	fi
+
 	cd - > /dev/null
 fi
