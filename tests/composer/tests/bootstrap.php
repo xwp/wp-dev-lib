bb9935c722ddffd9751c27ecb4b8b9fa211df865
Kaspars Dambis <hi@kaspars.net>
2018-06-02 11:35:44 +0300

    Remove debugging

--- tests/composer/tests/bootstrap.php
+++ tests/composer/tests/bootstrap.php
@@ -9,11 +9,5 @@ if ( ! file_exists( $_tests_dir . '/includes' ) ) {
 // @see https://core.trac.wordpress.org/browser/trunk/tests/phpunit/includes/functions.php
 require_once $_tests_dir . '/includes/functions.php';
 
-tests_add_filter( 'muplugins_loaded', function() {
-	global $wpdb;
-	$results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}options" );
-	var_dump( $results );
-} );
-
 // @see https://core.trac.wordpress.org/browser/trunk/tests/phpunit/includes/bootstrap.php
 require $_tests_dir . '/includes/bootstrap.php';
