257487a542c72d75aacf68dcbb1b57da38f33b62
Kaspars Dambis <hi@kaspars.net>
2018-06-02 12:06:19 +0300

    Debug DB queries during tests

--- tests/composer/tests/phpunit/sample.test.php
+++ tests/composer/tests/phpunit/sample.test.php
@@ -6,6 +6,9 @@
 class Sample_Test extends WP_UnitTestCase {
 
 	function test_tests() {
+		global $wpdb;
+		$results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}options" );
+		print_r( $results );
 		$this->assertTrue( true );
 	}
 
