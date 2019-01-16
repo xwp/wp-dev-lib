2d664090f6c66548c285229ab18ef13c13a054ce
Kaspars Dambis <hi@kaspars.net>
2019-01-16 08:51:50 +0200

    Simplify the demo test

--- tests/composer/tests/phpunit/sample.test.php
+++ tests/composer/tests/phpunit/sample.test.php
@@ -6,9 +6,7 @@
 class Sample_Test extends WP_UnitTestCase {
 
 	function test_tests() {
-		global $wpdb;
-		$results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}options" );
-		$this->assertTrue( true );
+		$this->assertContains( 'http', site_url(), 'Site URL contains a valid protocol.' );
 	}
 
 }
