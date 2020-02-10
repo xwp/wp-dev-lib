c8caa3df0eb73e3a8d4048bd8cc083b8b3e7ee50
Kaspars Dambis <hi@kaspars.net>
2020-02-10 11:19:26 +0200

    Make it generic and run first

--- scripts/travis.script.sh
+++ scripts/travis.script.sh
@@ -8,13 +8,15 @@ if [[ -z $SKIP_ECHO_PATHS_SCOPE ]] && [[ $CHECK_SCOPE != "all" ]]; then
 fi
 echo
 
+# Run any custom checks by defining a run_tests function, see sample-scripts/.dev-lib
+if [ "$( type -t run_tests )" != '' ]; then
+	run_tests
+fi
+
 check_execute_bit
 lint_js_files
 lint_php_files
 lint_xml_files
 run_qunit
-if [ "$( type -t run_tests )" != '' ]; then
-	run_tests
-fi
 run_phpunit_travisci
 run_codeception
