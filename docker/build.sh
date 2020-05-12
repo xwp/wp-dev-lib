f9b480ee98144bf5b15db752220d5a562834550a
Kaspars Dambis <hi@kaspars.net>
2020-05-12 10:50:45 +0300

    Allow this to be run from anywhere

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 IMAGE_NAME="xwpco/wp-dev-lib"
+IMAGE_SRC_DIR="$(dirname "$0")/wp-dev-lib"
+
 REPO_TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
 WP_DEV_LIB_VERSION=${WP_DEV_LIB_VERSION:-$REPO_TAG}
 
@@ -9,5 +11,5 @@ docker build \
 	--build-arg WP_PHP_VERSION=7.4 \
 	--tag "$IMAGE_NAME:$WP_DEV_LIB_VERSION" \
 	--tag "$IMAGE_NAME:$WP_DEV_LIB_VERSION-php7.4" \
-	./wp-dev-lib
+	"$IMAGE_SRC_DIR"
 
